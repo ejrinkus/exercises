e9567be3a76af39fd8ccf9c5f827695882a3c929
Eric Rinkus <ejrinkus@gmail.com>
2020-12-03 03:31:32 -0700

    aoc2015, day 8, part 2

--- aoc2015/examples/day8/day.rs
+++ aoc2015/examples/day8/day.rs
@@ -26,8 +26,22 @@ pub fn part_one(input: &str) -> i64 {
 }
 
 pub fn part_two(input: &str) -> i64 {
-  println!("{}", input);
-  0
+  let mut code_count = 0;
+  let mut enc_count = 0;
+  for line in input.lines() {
+    let mut length = line.len();
+    code_count += length;
+    length += 2; // For the new pair of quotes to surround the encoded string.
+    for c in line.chars() {
+      if c == '\\' {
+        length += 1; // To escape this backslash.
+      } else if c == '\"' {
+        length += 1; // to escape this quote.
+      }
+    }
+    enc_count += length;
+  }
+  enc_count as i64 - code_count as i64
 }
 
 #[cfg(test)]
@@ -49,6 +63,14 @@ mod day8_tests {
 
   #[test]
   fn samples_part2() {
-    assert_eq!(part_two(""), 0);
+    assert_eq!(
+      part_two(
+        "\"\"
+\"abc\"
+\"aaa\\\"aaa\"
+\"\\x27\""
+      ),
+      19
+    );
   }
 }
