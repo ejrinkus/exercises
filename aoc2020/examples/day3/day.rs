c091e8f7f10fbc011f4c1bfd48186f15d2537d29
Eric Rinkus <ejrinkus@gmail.com>
2020-12-02 22:56:06 -0700

    aoc2020, day 3, both parts

--- aoc2020/examples/day3/day.rs
+++ aoc2020/examples/day3/day.rs
@@ -1,11 +1,36 @@
 pub fn part_one(input: &str) -> i64 {
-  println!("{}", input);
-  0
+  check_slope(input, 3, 1)
 }
 
 pub fn part_two(input: &str) -> i64 {
-  println!("{}", input);
-  0
+  check_slope(input, 1, 1)
+    * check_slope(input, 3, 1)
+    * check_slope(input, 5, 1)
+    * check_slope(input, 7, 1)
+    * check_slope(input, 1, 2)
+}
+
+pub fn check_slope(input: &str, right: usize, down: usize) -> i64 {
+  let width = input.lines().next().unwrap().chars().count();
+  let height = input.lines().count();
+  let mut step = 1;
+  let mut count = 0;
+  loop {
+    // Figure out the row and column we need to seek to.
+    let row_i = (step * down) % height;
+    let col_i = (step * right) % width;
+
+    // Seek to the correct position.
+    let row = input.lines().skip(row_i).next().unwrap();
+    let pos = row.chars().skip(col_i).next().unwrap();
+    if pos == '#' {
+      count += 1;
+    }
+    if row_i == height - 1 {
+      return count;
+    }
+    step += 1;
+  }
 }
 
 #[cfg(test)]
@@ -14,11 +39,41 @@ mod day3_tests {
 
   #[test]
   fn samples_part1() {
-    assert_eq!(part_one(""), 0);
+    assert_eq!(
+      part_one(
+        "..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#"
+      ),
+      7
+    );
   }
 
   #[test]
   fn samples_part2() {
-    assert_eq!(part_two(""), 0);
+    assert_eq!(
+      part_two(
+        "..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#"
+      ),
+      336
+    );
   }
 }
