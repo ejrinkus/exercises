c53d3a81fb9c1f82cc24ff196bb50e8175d59d35
Eric Rinkus <ejrinkus@gmail.com>
2020-12-04 03:01:01 -0700

    aoc2015, day 10, both parts

--- aoc2015/examples/day10/day.rs
+++ aoc2015/examples/day10/day.rs
@@ -1,11 +1,35 @@
-pub fn part_one(input: &str) -> i64 {
-  println!("{}", input);
-  0
+pub fn look_and_say(input: &str) -> String {
+  let mut output = String::with_capacity(input.len() * 2);
+  let mut curr_char = input.chars().next().unwrap();
+  let mut last_i = 0;
+  for (i, c) in input.chars().enumerate() {
+    if c == curr_char {
+      continue;
+    }
+    let diff = i - last_i;
+    output.push_str(&diff.to_string());
+    output.push(curr_char);
+    curr_char = c;
+    last_i = i
+  }
+  output = format!("{}{}{}", output, input.len() - last_i, curr_char);
+  output
 }
 
-pub fn part_two(input: &str) -> i64 {
-  println!("{}", input);
-  0
+pub fn part_one(input: &str) -> usize {
+  let mut result = input.to_string();
+  for _i in 0..40 {
+    result = look_and_say(&result);
+  }
+  result.len()
+}
+
+pub fn part_two(input: &str) -> usize {
+  let mut result = input.to_string();
+  for _i in 0..50 {
+    result = look_and_say(&result);
+  }
+  result.len()
 }
 
 #[cfg(test)]
