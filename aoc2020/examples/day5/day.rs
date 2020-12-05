9358168714d865faab29037c3e4fe0c56f45bba5
Eric Rinkus <ejrinkus@gmail.com>
2020-12-04 22:56:12 -0700

    aoc2020, day 5, cooler part 2 solution

--- aoc2020/examples/day5/day.rs
+++ aoc2020/examples/day5/day.rs
@@ -11,7 +11,7 @@ pub fn get_seat(input: &str) -> (u32, u32, u32) {
   (row, col, row * 8 + col)
 }
 
-pub fn part_one(input: &str) -> i64 {
+pub fn part_one(input: &str) -> u32 {
   let mut highest_id = 0;
   for line in input.lines() {
     let assignment = get_seat(line);
@@ -19,23 +19,43 @@ pub fn part_one(input: &str) -> i64 {
       highest_id = assignment.2;
     }
   }
-  highest_id as i64
+  highest_id
 }
 
-pub fn part_two(input: &str) -> i64 {
+// Used this in my original solution...
+pub fn part_two_old(input: &str) -> u32 {
   let mut ids: Vec<u32> = Vec::new();
   for line in input.lines() {
     let assignment = get_seat(line);
     ids.push(assignment.2);
   }
   ids.sort();
-  let mut result = -1;
+  let mut result = 0;
   for i in 1..ids.len() - 1 {
     if ids[i] + 1 != ids[i + 1] {
-      result = ids[i] as i64 + 1;
+      result = ids[i] + 1;
     }
   }
   result
+}
+
+// ...but this is way slicker :)
+pub fn part_two(input: &str) -> u32 {
+  let mut id_sum = 0_u32;
+  let mut min_id = u32::MAX;
+  let mut max_id = 0_u32;
+  for line in input.lines() {
+    let assignment = get_seat(line);
+    id_sum += assignment.2;
+    if assignment.2 < min_id {
+      min_id = assignment.2;
+    }
+    if assignment.2 > max_id {
+      max_id = assignment.2;
+    }
+  }
+  let expected_sum = ((max_id * (max_id + 1)) / 2) - (((min_id - 1) * (min_id)) / 2);
+  expected_sum - id_sum
 }
 
 #[cfg(test)]
