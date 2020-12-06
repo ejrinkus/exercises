97352180b4ac3fba727be962b336d01beb1ee391
Eric Rinkus <ejrinkus@gmail.com>
2020-12-05 22:14:55 -0700

    aoc2020, day 6, both parts

--- aoc2020/examples/day6/day.rs
+++ aoc2020/examples/day6/day.rs
@@ -1,11 +1,49 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
+
 pub fn part_one(input: &str) -> i64 {
-  println!("{}", input);
-  0
+  let mut set: HashSet<char> = HashSet::new();
+  let mut count = 0;
+  for line in input.lines() {
+    if line.trim().is_empty() {
+      count += set.len();
+      set.clear();
+    }
+    for c in line.trim().chars() {
+      set.insert(c);
+    }
+  }
+  count += set.len();
+  count as i64
 }
 
 pub fn part_two(input: &str) -> i64 {
-  println!("{}", input);
-  0
+  let mut set: HashMap<char, i64> = HashMap::new();
+  let mut count = 0;
+  let mut group_size = 0;
+  for line in input.lines() {
+    if line.trim().is_empty() {
+      for (_k, v) in set.clone() {
+        if v == group_size {
+          count += 1;
+        }
+      }
+      group_size = 0;
+      set.clear();
+      continue;
+    }
+    for c in line.trim().chars() {
+      let e = set.entry(c).or_insert(0);
+      *e += 1;
+    }
+    group_size += 1;
+  }
+  for (_k, v) in set {
+    if v == group_size {
+      count += 1;
+    }
+  }
+  count as i64
 }
 
 #[cfg(test)]
