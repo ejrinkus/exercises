ae769e8ae9f0513433c2125491bdf15749c85008
Eric Rinkus <ejrinkus@gmail.com>
2020-12-11 02:01:48 -0700

    aoc2020, day 11, part 2

--- aoc2020/examples/day11/day.rs
+++ aoc2020/examples/day11/day.rs
@@ -49,19 +49,31 @@ impl SeatMap {
       if *s == State::Floor {
         continue;
       }
-      let adjacents = self.get_adjacents(i);
-      let mut neighbors = 0;
-      for maybe_a in adjacents.iter() {
-        if let Some(j) = maybe_a {
-          if self.seats.get(*j) == Some(&State::Occupied) {
-            neighbors += 1;
-          }
-        }
-      }
-      if neighbors == 0 && *s == State::Empty {
+      let adjacents = self.count_adjacents(i);
+      if adjacents == 0 && *s == State::Empty {
         new_seats[i] = State::Occupied;
         changed = true;
-      } else if neighbors >= 4 && *s == State::Occupied {
+      } else if adjacents >= 4 && *s == State::Occupied {
+        new_seats[i] = State::Empty;
+        changed = true;
+      }
+    }
+    self.seats = new_seats;
+    changed
+  }
+
+  pub fn simulate2(&mut self) -> bool {
+    let mut changed = false;
+    let mut new_seats = self.seats.clone();
+    for (i, s) in self.seats.iter().enumerate() {
+      if *s == State::Floor {
+        continue;
+      }
+      let adjacents = self.count_adjacents2(i);
+      if adjacents == 0 && *s == State::Empty {
+        new_seats[i] = State::Occupied;
+        changed = true;
+      } else if adjacents >= 5 && *s == State::Occupied {
         new_seats[i] = State::Empty;
         changed = true;
       }
@@ -83,40 +95,138 @@ impl SeatMap {
     )
   }
 
-  fn get_adjacents(&self, i: usize) -> [Option<usize>; 8] {
-    let mut adjacents: [Option<usize>; 8] = [None; 8];
-    if i >= self.width {
-      if i % self.width != 0 {
-        // Up-left
-        adjacents[0] = Some(i - self.width - 1);
-      }
-      // Up
-      adjacents[1] = Some(i - self.width);
-      if i % self.width != self.width - 1 {
-        // Up-right
-        adjacents[2] = Some(i - self.width + 1);
-      }
+  fn count_adjacents(&self, i: usize) -> u8 {
+    let mut adjacents = 0;
+    let up_left = i as i32 - self.width as i32 - 1;
+    let up = i as i32 - self.width as i32;
+    let up_right = i as i32 - self.width as i32 + 1;
+    let down_left = i as i32 + self.width as i32 - 1;
+    let down = i as i32 + self.width as i32;
+    let down_right = i as i32 + self.width as i32 + 1;
+    let left = i as i32 - 1;
+    let right = i as i32 + 1;
+
+    if up_left >= 0 && i % self.width != 0 {
+      adjacents += (*self.seats.get(up_left as usize).unwrap() == State::Occupied) as u8;
     }
-    if i < self.seats.len() - self.width {
-      if i % self.width != 0 {
-        // Down-left
-        adjacents[6] = Some(i + self.width - 1);
-      }
-      // Down
-      adjacents[5] = Some(i + self.width);
-      if i % self.width != self.width - 1 {
-        // Down-right
-        adjacents[4] = Some(i + self.width + 1);
-      }
+    if up >= 0 {
+      adjacents += (*self.seats.get(up as usize).unwrap() == State::Occupied) as u8;
+    }
+    if up_right >= 0 && i % self.width != self.width - 1 {
+      adjacents += (*self.seats.get(up_right as usize).unwrap() == State::Occupied) as u8;
+    }
+    if down_left < self.seats.len() as i32 && i % self.width != 0 {
+      adjacents += (*self.seats.get(down_left as usize).unwrap() == State::Occupied) as u8;
+    }
+    if down < self.seats.len() as i32 {
+      adjacents += (*self.seats.get(down as usize).unwrap() == State::Occupied) as u8;
+    }
+    if down_right < self.seats.len() as i32 && i % self.width != self.width - 1 {
+      adjacents += (*self.seats.get(down_right as usize).unwrap() == State::Occupied) as u8;
     }
     if i % self.width != 0 {
-      // Left
-      adjacents[7] = Some(i - 1);
+      adjacents += (*self.seats.get(left as usize).unwrap() == State::Occupied) as u8;
     }
     if i % self.width != self.width - 1 {
-      // Right
-      adjacents[3] = Some(i + 1);
+      adjacents += (*self.seats.get(right as usize).unwrap() == State::Occupied) as u8;
     }
+
+    adjacents
+  }
+
+  fn count_adjacents2(&self, i: usize) -> u8 {
+    let mut adjacents = 0;
+
+    let mut up_left = i as i32 - self.width as i32 - 1;
+    let mut up = i as i32 - self.width as i32;
+    let mut up_right = i as i32 - self.width as i32 + 1;
+    let mut down_left = i as i32 + self.width as i32 - 1;
+    let mut down = i as i32 + self.width as i32;
+    let mut down_right = i as i32 + self.width as i32 + 1;
+    let mut left = i as i32 - 1;
+    let mut right = i as i32 + 1;
+
+    while up_left >= 0 && up_left as usize as usize % self.width != self.width - 1 {
+      match self.seats.get(up_left as usize).unwrap() {
+        State::Floor => up_left = up_left - self.width as i32 - 1,
+        State::Empty => break,
+        State::Occupied => {
+          adjacents += 1;
+          break;
+        }
+      }
+    }
+    while up >= 0 {
+      match self.seats.get(up as usize).unwrap() {
+        State::Floor => up = up - self.width as i32,
+        State::Empty => break,
+        State::Occupied => {
+          adjacents += 1;
+          break;
+        }
+      }
+    }
+    while up_right >= 0 && up_right as usize % self.width != 0 {
+      match self.seats.get(up_right as usize).unwrap() {
+        State::Floor => up_right = up_right - self.width as i32 + 1,
+        State::Empty => break,
+        State::Occupied => {
+          adjacents += 1;
+          break;
+        }
+      }
+    }
+    while down_left < self.seats.len() as i32 && down_left as usize % self.width != self.width - 1 {
+      match self.seats.get(down_left as usize).unwrap() {
+        State::Floor => down_left = down_left + self.width as i32 - 1,
+        State::Empty => break,
+        State::Occupied => {
+          adjacents += 1;
+          break;
+        }
+      }
+    }
+    while down < self.seats.len() as i32 {
+      match self.seats.get(down as usize).unwrap() {
+        State::Floor => down = down + self.width as i32,
+        State::Empty => break,
+        State::Occupied => {
+          adjacents += 1;
+          break;
+        }
+      }
+    }
+    while down_right < self.seats.len() as i32 && down_right as usize % self.width != 0 {
+      match self.seats.get(down_right as usize).unwrap() {
+        State::Floor => down_right = down_right + self.width as i32 + 1,
+        State::Empty => break,
+        State::Occupied => {
+          adjacents += 1;
+          break;
+        }
+      }
+    }
+    while left >= 0 && left as usize % self.width != self.width - 1 {
+      match self.seats.get(left as usize).unwrap() {
+        State::Floor => left = left - 1,
+        State::Empty => break,
+        State::Occupied => {
+          adjacents += 1;
+          break;
+        }
+      }
+    }
+    while (right as usize) < self.seats.len() && right as usize % self.width != 0 {
+      match self.seats.get(right as usize).unwrap() {
+        State::Floor => right = right + 1,
+        State::Empty => break,
+        State::Occupied => {
+          adjacents += 1;
+          break;
+        }
+      }
+    }
+
     adjacents
   }
 }
@@ -127,9 +237,10 @@ pub fn part_one(input: &str) -> u32 {
   seats.count_occupied()
 }
 
-pub fn part_two(input: &str) -> i64 {
-  println!("{}", input);
-  0
+pub fn part_two(input: &str) -> u32 {
+  let mut seats = SeatMap::new(input);
+  while seats.simulate2() {}
+  seats.count_occupied()
 }
 
 #[cfg(test)]
@@ -157,6 +268,20 @@ L.LLLLL.LL"
 
   #[test]
   fn samples_part2() {
-    assert_eq!(part_two(""), 0);
+    assert_eq!(
+      part_two(
+        "L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL"
+      ),
+      26
+    );
   }
 }
