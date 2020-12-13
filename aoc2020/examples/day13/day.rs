12ecf61f246b8a66d36888addc79881d094534e7
Eric Rinkus <ejrinkus@gmail.com>
2020-12-13 03:51:14 -0700

    aoc2020, day 13, part 2

--- aoc2020/examples/day13/day.rs
+++ aoc2020/examples/day13/day.rs
@@ -21,20 +21,16 @@ pub fn part_one(input: &str) -> u64 {
   min_id * min_diff
 }
 
-pub fn find_ts(offset: u64, id: u64, factor: u64, prev: &Vec<(u64, u64)>) -> u64 {
-  let mut maybe_ts = factor;
+// The solution for IDs a and b is some number x in the range 0..lcm, where lcm
+// is the lowest common multiple of a and b.  Additional solutions can be found
+// using the formula lcm*i + x, where i is any integer.
+pub fn find_ts(offset: u64, id: u64, base: u64, prev_lcm: u64) -> u64 {
+  let mut maybe_ts = base;
   loop {
-    let mut solved = true;
-    for (o, n) in prev {
-      if (maybe_ts + o) % n != 0 {
-        solved = false;
-        break;
-      }
-    }
-    if solved && (maybe_ts + offset) % id == 0 {
+    if (maybe_ts + offset) % id == 0 {
       return maybe_ts;
     }
-    maybe_ts += factor;
+    maybe_ts += prev_lcm;
   }
 }
 
@@ -50,43 +46,14 @@ pub fn part_two(input: &str) -> u64 {
     enumerated_ids.push((offset as u64, id.parse::<u64>().unwrap()));
   }
   enumerated_ids.sort_by(|a, b| a.1.cmp(&b.1));
-  let mut solved_ids: Vec<(u64, u64)> = Vec::new();
   let mut timestamp = 1;
+  let mut prev_lcm = 1;
   for (o, id) in enumerated_ids {
-    timestamp = find_ts(o, id, timestamp, &solved_ids);
-    solved_ids.push((o, id));
+    timestamp = find_ts(o, id, timestamp, prev_lcm);
+    prev_lcm *= id;
     println!("up to id {}, offset {}, solution is {}", id, o, timestamp);
   }
   timestamp
-  // // LCM of primes is the product of primes.  The target timestamp must be less
-  // // than the LCM of all the bus IDs
-  // let lcm = enumerated_ids.iter().fold(1, |acc, (_o, n)| acc * n);
-  // // Similarly, the target timestamp must be _greater_ than the LCM of the bus
-  // // IDs _except_ for the largest.
-  // let lcm_lower = enumerated_ids
-  //   .iter()
-  //   .take(enumerated_ids.len() - 1)
-  //   .fold(1, |acc, (_o, n)| acc * n);
-  // println!("range ({}, {})", lcm - lcm_lower, lcm);
-
-  // let max_o = enumerated_ids.last().unwrap().0;
-  // let max = enumerated_ids.last().unwrap().1;
-  // let min = enumerated_ids.first().unwrap().1;
-  // let mut timestamp = ((lcm - lcm_lower) / max) * max;
-  // while timestamp < lcm {
-  //   let mut solved = true;
-  //   for (o, n) in &enumerated_ids {
-  //     if (timestamp - max_o + *o as u64) % n != 0 {
-  //       solved = false;
-  //       break;
-  //     }
-  //   }
-  //   if solved {
-  //     return timestamp - max_o;
-  //   }
-  //   timestamp += max;
-  // }
-  // 0
 }
 
 #[cfg(test)]
@@ -115,10 +82,3 @@ mod day13_tests {
     );
   }
 }
-
-/*
-target: 1068781
-
-lcm: 3162341
-lcm_lower: 166439
-*/
