9563256f5d10c845a12a29af07ee2dd68aac019e
Eric Rinkus <ejrinkus@gmail.com>
2020-12-25 01:42:50 -0700

    aoc2020, day 25

--- aoc2020/examples/day25/day.rs
+++ aoc2020/examples/day25/day.rs
@@ -1,6 +1,23 @@
 pub fn part_one(input: &str) -> i64 {
-  println!("{}", input);
-  0
+  let mut lines = input.lines();
+  let card_pub = lines.next().unwrap().parse::<i64>().unwrap();
+  let door_pub = lines.next().unwrap().parse::<i64>().unwrap();
+  let mut card_loop = 1;
+  let mut maybe_pub = 1;
+  loop {
+    maybe_pub = (maybe_pub * 7) % 20201227;
+    if maybe_pub == card_pub {
+      break;
+    } else {
+      card_loop += 1;
+    }
+  }
+  let mut value = 1;
+  for _ in 0..card_loop {
+    value *= door_pub;
+    value %= 20201227;
+  }
+  value
 }
 
 pub fn part_two(input: &str) -> i64 {
@@ -14,7 +31,7 @@ mod day25_tests {
 
   #[test]
   fn samples_part1() {
-    assert_eq!(part_one(""), 0);
+    assert_eq!(part_one("5764801\n17807724"), 14897079);
   }
 
   #[test]
