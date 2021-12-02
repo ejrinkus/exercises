961503dbfffbc0d76d62a6b078b94a78cf0e976f
Eric Rinkus <ejrinkus@gmail.com>
2021-12-01 20:39:09 -0500

    initialize aoc2021

---
+++ aoc2021/examples/day25/day.rs
@@ -0,0 +1,24 @@
+pub fn part_one(input: &str) -> i64 {
+  println!("{}", input);
+  0
+}
+
+pub fn part_two(input: &str) -> i64 {
+  println!("{}", input);
+  0
+}
+
+#[cfg(test)]
+mod day25_tests {
+  use super::*;
+
+  #[test]
+  fn samples_part1() {
+    assert_eq!(part_one(""), 0);
+  }
+
+  #[test]
+  fn samples_part2() {
+    assert_eq!(part_two(""), 0);
+  }
+}
