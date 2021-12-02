8d0fc93c90bd9ca86d26371ce2b18461dac23f47
Eric Rinkus <ejrinkus@gmail.com>
2021-12-01 21:41:14 -0500

    aoc2021 day1 solutions

--- aoc2021/examples/day1/day.rs
+++ aoc2021/examples/day1/day.rs
@@ -1,11 +1,22 @@
 pub fn part_one(input: &str) -> i64 {
-  println!("{}", input);
-  0
+  solution(input, 1)
 }
 
 pub fn part_two(input: &str) -> i64 {
-  println!("{}", input);
-  0
+  solution(input, 3)
+}
+
+pub fn solution(input: &str, skips: usize) -> i64 {
+  let mut count = 0i64;
+  input
+    .lines()
+    .zip(input.lines().skip(skips))
+    .for_each(|(x, y)| {
+      if x.parse::<i64>().unwrap() < y.parse::<i64>().unwrap() {
+        count += 1;
+      }
+    });
+  count
 }
 
 #[cfg(test)]
@@ -14,11 +25,39 @@ mod day1_tests {
 
   #[test]
   fn samples_part1() {
-    assert_eq!(part_one(""), 0);
+    assert_eq!(
+      part_one(
+        "199
+200
+208
+210
+200
+207
+240
+269
+260
+263"
+      ),
+      7
+    );
   }
 
   #[test]
   fn samples_part2() {
-    assert_eq!(part_two(""), 0);
+    assert_eq!(
+      part_two(
+        "199
+200
+208
+210
+200
+207
+240
+269
+260
+263"
+      ),
+      5
+    );
   }
 }
