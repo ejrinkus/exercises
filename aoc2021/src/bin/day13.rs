3064bbb16861590224798cd00d9c7ce344ec3762
Eric Rinkus <ejrinkus@gmail.com>
2021-12-13 01:19:27 -0700

    aoc2021 day13

--- aoc2021/src/bin/day13.rs
+++ aoc2021/src/bin/day13.rs
@@ -1,4 +1,6 @@
 use aoc_helpers::*;
+use std::collections::HashSet;
+use std::vec::Vec;
 
 const YEAR: u32 = 2021;
 const DAY: u32 = 13;
@@ -21,12 +23,102 @@ fn main() {
   }
 }
 
-pub fn part_one(_input: &str) -> i64 {
-  0
+enum Direction {
+  X,
+  Y
 }
 
-pub fn part_two(_input: &str) -> i64 {
-  0
+struct Page {
+  dots: HashSet<(usize, usize)>,
+  folds: Vec<(Direction, usize)>,
+}
+
+impl Page {
+  pub fn new(input: &str) -> Page {
+    let mut lines = input.lines();
+    let mut dotline = lines.next().unwrap().trim();
+    let mut dots: HashSet<(usize, usize)> = HashSet::new();
+    while dotline != "" {
+      let mut coords = dotline.split(",");
+      let (x, y) = (coords.next().unwrap(), coords.next().unwrap());
+      dots.insert((x.parse::<usize>().unwrap(), y.parse::<usize>().unwrap()));
+      dotline = lines.next().unwrap().trim();
+    }
+    let mut folds: Vec<(Direction, usize)> = Vec::new();
+    for line in lines {
+      let replaced = line.replace("fold along ", "");
+      let mut parts = replaced.split("=");
+      let dir = parts.next().unwrap();
+      let val = parts.next().unwrap().parse::<usize>().unwrap();
+      match dir {
+        "x" => folds.push((Direction::X, val)),
+        "y" => folds.push((Direction::Y, val)),
+        _ => panic!("unexpected direction value {}", dir)
+      }
+    }
+    Page {
+      dots: dots,
+      folds: folds
+    }
+  }
+
+  pub fn fold_once(&mut self, i: usize) {
+    let (dir, val) = &self.folds[i];
+    let mut newdots: HashSet<(usize, usize)> = HashSet::new();
+    for (x, y) in &self.dots {
+      match dir {
+        Direction::X => {
+          if x < val {
+            newdots.insert((*x, *y));
+          } else {
+            newdots.insert(((2*val)-x, *y));
+          }
+        },
+        Direction::Y => {
+          if y < val {
+            newdots.insert((*x, *y));
+          } else {
+            newdots.insert((*x, (2*val)-y));
+          }
+        }
+      }
+    }
+    self.dots = newdots;
+  }
+
+  pub fn fold_all(&mut self) {
+    for i in 0..self.folds.len() {
+      self.fold_once(i);
+    }
+  }
+}
+
+pub fn part_one(input: &str) -> usize {
+  let mut page = Page::new(input);
+  page.fold_once(0);
+  page.dots.len()
+}
+
+pub fn part_two(input: &str) -> usize {
+  let mut page = Page::new(input);
+  page.fold_all();
+  let mut grid: Vec<Vec<char>> = Vec::new();
+  for (x, y) in &page.dots {
+    while grid.len() <= *y {
+      grid.push(Vec::new());
+    }
+    while grid[*y].len() <= *x {
+      grid[*y].push('.');
+    }
+    grid[*y][*x] = '#';
+  }
+  for row in grid {
+    for c in row {
+      print!("{}", c);
+    }
+    println!("");
+  }
+  page.dots.len()
 }
 
 #[cfg(test)]
@@ -35,11 +127,51 @@ mod day2_tests {
 
   #[test]
   fn samples_part1() {
-    assert_eq!(part_one(""), 0);
+    assert_eq!(part_one("6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5"), 17);
   }
 
   #[test]
   fn samples_part2() {
-    assert_eq!(part_two(""), 0);
+    assert_eq!(part_two("6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5"), 16);
   }
 }
