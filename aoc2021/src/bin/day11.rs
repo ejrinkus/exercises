f64f9bcefd4aaf2e96e76aa8976121cb34c5c565
Eric Rinkus <ejrinkus@gmail.com>
2021-12-12 23:31:51 -0700

    aoc2021 day11

--- aoc2021/src/bin/day11.rs
+++ aoc2021/src/bin/day11.rs
@@ -1,4 +1,5 @@
 use aoc_helpers::*;
+use std::vec::Vec;
 
 const YEAR: u32 = 2021;
 const DAY: u32 = 11;
@@ -21,12 +22,134 @@ fn main() {
   }
 }
 
-pub fn part_one(_input: &str) -> i64 {
-  0
+struct OctoGrid {
+  grid: Vec<Vec<u8>>,
+  width: usize,
+  height: usize,
+  bright_bois: Vec<(usize, usize)>,
+  flashes: u64
 }
 
-pub fn part_two(_input: &str) -> i64 {
-  0
+impl OctoGrid {
+  pub fn new(input: &str) -> OctoGrid {
+    let mut octos = Vec::new();
+    for line in input.lines() {
+      let mut row: Vec<u8> = Vec::new();
+      for c in line.chars() {
+        row.push(c.to_digit(10).unwrap() as u8);
+      }
+      octos.push(row);
+    }
+    let (w, h) = (octos[0].len(), octos.len());
+    OctoGrid {
+      grid: octos,
+      width: w,
+      height: h,
+      bright_bois: Vec::new(),
+      flashes: 0
+    }
+  }
+
+  fn increment(&mut self) {
+    for (y, row) in self.grid.iter_mut().enumerate() {
+      for (x, octo) in row.iter_mut().enumerate() {
+        *octo += 1;
+        if *octo > 9 {
+          self.bright_bois.push((x, y));
+        }
+      }
+    }
+  }
+
+  fn flash(&mut self) {
+    let mut i = 0usize;
+    while i < self.bright_bois.len() {
+      let (x, y) = self.bright_bois[i];
+      if x > 0 {
+        if y > 0 {
+          self.grid[y-1][x-1] += 1;
+          if self.grid[y-1][x-1] == 10 {
+            self.bright_bois.push((x-1, y-1));
+          }
+        }
+        self.grid[y][x-1] += 1;
+        if self.grid[y][x-1] == 10 {
+          self.bright_bois.push((x-1, y));
+        }
+        if y < self.height-1 {
+          self.grid[y+1][x-1] += 1;
+          if self.grid[y+1][x-1] == 10 {
+            self.bright_bois.push((x-1, y+1));
+          }
+        }
+      }
+      if x < self.width-1 {
+        if y > 0 {
+          self.grid[y-1][x+1] += 1;
+          if self.grid[y-1][x+1] == 10 {
+            self.bright_bois.push((x+1, y-1));
+          }
+        }
+        self.grid[y][x+1] += 1;
+        if self.grid[y][x+1] == 10 {
+          self.bright_bois.push((x+1, y));
+        }
+        if y < self.height-1 {
+          self.grid[y+1][x+1] += 1;
+          if self.grid[y+1][x+1] == 10 {
+            self.bright_bois.push((x+1, y+1));
+          }
+        }
+      }
+      if y > 0 {
+        self.grid[y-1][x] += 1;
+        if self.grid[y-1][x] == 10 {
+          self.bright_bois.push((x, y-1));
+        }
+      }
+      if y < self.height-1 {
+        self.grid[y+1][x] += 1;
+        if self.grid[y+1][x] == 10 {
+          self.bright_bois.push((x, y+1));
+        }
+      }
+      i += 1;
+    }
+    while !self.bright_bois.is_empty() {
+      let (x, y) = self.bright_bois.pop().unwrap();
+      self.grid[y][x] = 0;
+      self.flashes += 1;
+    }
+  }
+
+  pub fn step(&mut self) {
+    self.increment();
+    self.flash();
+  }
+}
+
+pub fn part_one(input: &str) -> u64 {
+  let mut octos = OctoGrid::new(input);
+  for _i in 0..100 {
+    octos.step();
+  }
+  octos.flashes
+}
+
+pub fn part_two(input: &str) -> i64 {
+  let mut octos = OctoGrid::new(input);
+  let total_octos = (octos.width as u64) * (octos.height as u64);
+  let mut step = 1;
+  let mut flashes_before = 0;
+  loop {
+    octos.step();
+    if octos.flashes - flashes_before == total_octos {
+      break;
+    }
+    flashes_before = octos.flashes;
+    step += 1;
+  }
+  step
 }
 
 #[cfg(test)]
@@ -35,11 +158,29 @@ mod day2_tests {
 
   #[test]
   fn samples_part1() {
-    assert_eq!(part_one(""), 0);
+    assert_eq!(part_one("5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526"), 1656);
   }
 
   #[test]
   fn samples_part2() {
-    assert_eq!(part_two(""), 0);
+    assert_eq!(part_two("5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526"), 195);
   }
 }
