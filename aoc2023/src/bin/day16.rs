8e53c87ac253e3a39707486d361dae21ae75edd2
Eric Rinkus <ejrinkus@gmail.com>
2023-12-03 01:52:23 -0700

    finish initializing 2023

---
+++ aoc2023/src/bin/day16.rs
@@ -0,0 +1,43 @@
+use aoc_helpers::*;
+
+fn main() {
+    let solution = Solution {};
+    run(&solution);
+}
+
+struct Solution {}
+
+impl AocSolution for Solution {
+    fn year(&self) -> u32 {
+        2023
+    }
+    fn day(&self) -> u32 {
+        16
+    }
+
+    fn part_one(&self, _input: &str) -> String {
+        "".to_string()
+    }
+
+    fn part_two(&self, _input: &str) -> String {
+        "".to_string()
+    }
+}
+
+#[cfg(test)]
+mod day1_tests {
+    use super::*;
+
+    #[test]
+    fn samples_part1() {
+        let solution = Solution {};
+        assert_eq!(solution.part_one(""), "");
+    }
+
+    #[test]
+    fn samples_part2() {
+        let solution = Solution {};
+        assert_eq!(solution.part_two(""), "");
+    }
+}
+
