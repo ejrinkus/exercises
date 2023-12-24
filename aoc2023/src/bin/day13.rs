9b224a545b6bb9c3711558648ac146624ea4dc87
Eric Rinkus <ejrinkus@gmail.com>
2023-12-24 01:47:18 -0500

    aoc2023d13 part two

--- aoc2023/src/bin/day13.rs
+++ aoc2023/src/bin/day13.rs
@@ -24,9 +24,9 @@ impl AocSolution for Solution {
             if matrix.len() <= 0 {
                 break;
             }
-            let mut result = find_vertical_reflection(&matrix) + 1;
+            let mut result = find_vertical_reflection(&matrix, 0) + 1;
             if result == 0 {
-                result = 100 * (find_horizontal_reflection(&matrix) + 1);
+                result = 100 * (find_horizontal_reflection(&matrix, 0) + 1);
             }
             if result == 0 {
                 panic!("Couldn't find a reflection line");
@@ -36,18 +36,34 @@ impl AocSolution for Solution {
         sum.to_string()
     }
 
-    fn part_two(&self, _input: &str) -> String {
-        "".to_string()
+    fn part_two(&self, input: &str) -> String {
+        let mut lines = input.lines();
+        let mut sum = 0i32;
+        loop {
+            let matrix = matrix_from_lines(&mut lines);
+            if matrix.len() <= 0 {
+                break;
+            }
+            let mut result = find_vertical_reflection(&matrix, 1) + 1;
+            if result == 0 {
+                result = 100 * (find_horizontal_reflection(&matrix, 1) + 1);
+            }
+            if result == 0 {
+                panic!("Couldn't find a reflection line");
+            }
+            sum += result;
+        }
+        sum.to_string()
     }
 }
 
-fn find_vertical_reflection(matrix: &Vec<Vec<char>>) -> i32 {
+fn find_vertical_reflection(matrix: &Vec<Vec<char>>, smudges: i32) -> i32 {
     let last_col = matrix[0].len() - 1;
     // Either the left or right column _must_ be included in the reflection set.
     // Start with the left.
     let mut right = last_col;
     while right > 0 {
-        if col_range_is_reflection(matrix, 0, right) {
+        if col_range_is_reflection(matrix, 0, right, smudges) {
             return (right / 2) as i32;
         }
         right -= 1;
@@ -57,7 +73,7 @@ fn find_vertical_reflection(matrix: &Vec<Vec<char>>) -> i32 {
     // the right column.
     let mut left = 0;
     while left < last_col {
-        if col_range_is_reflection(matrix, left, last_col) {
+        if col_range_is_reflection(matrix, left, last_col, smudges) {
             return (((last_col - left) / 2) + left) as i32;
         }
         left += 1;
@@ -65,39 +81,46 @@ fn find_vertical_reflection(matrix: &Vec<Vec<char>>) -> i32 {
     return -1;
 }
 
-fn col_range_is_reflection(matrix: &Vec<Vec<char>>, mut left: usize, mut right: usize) -> bool {
+fn col_range_is_reflection(matrix: &Vec<Vec<char>>, mut left: usize, mut right: usize, smudges: i32) -> bool {
     if (right - left) % 2 == 0 {
         // This means that the "center" of this range is a single column.
         // The center of a reflection must be _between_ two columns though, so
         // this range cannot be valid.
         return false;
     }
+
+    let mut remaining = smudges;
     while left < right {
-        if !columns_equal(matrix, left, right) {
+        remaining = columns_equal(matrix, left, right, remaining);
+        if remaining < 0 {
             return false;
         }
         left += 1;
         right -= 1;
     }
-    true
+    remaining == 0
 }
 
-fn columns_equal(matrix: &Vec<Vec<char>>, left: usize, right: usize) -> bool {
+fn columns_equal(matrix: &Vec<Vec<char>>, left: usize, right: usize, smudges: i32) -> i32 {
+    let mut remaining = smudges;
     for row in 0..matrix.len() {
         if matrix[row][left] != matrix[row][right] {
-            return false;
+            if remaining == 0 {
+                return -1;
+            }
+            remaining -= 1;
         }
     }
-    true
+    remaining
 }
 
-fn find_horizontal_reflection(matrix: &Vec<Vec<char>>) -> i32 {
+fn find_horizontal_reflection(matrix: &Vec<Vec<char>>, smudges: i32) -> i32 {
     let last_row = matrix.len() - 1;
     // Either the top or bototm column _must_ be included in the reflection set.
     // Start with the top.
     let mut bottom = last_row;
     while bottom > 0 {
-        if row_range_is_reflection(matrix, 0, bottom) {
+        if row_range_is_reflection(matrix, 0, bottom, smudges) {
             return (bottom / 2) as i32;
         }
         bottom -= 1;
@@ -107,7 +130,7 @@ fn find_horizontal_reflection(matrix: &Vec<Vec<char>>) -> i32 {
     // the bottom row.
     let mut top = 0;
     while top < last_row {
-        if row_range_is_reflection(matrix, top, last_row) {
+        if row_range_is_reflection(matrix, top, last_row, smudges) {
             return (((last_row - top) / 2) + top) as i32;
         }
         top += 1;
@@ -115,30 +138,37 @@ fn find_horizontal_reflection(matrix: &Vec<Vec<char>>) -> i32 {
     return -1;
 }
 
-fn row_range_is_reflection(matrix: &Vec<Vec<char>>, mut top: usize, mut bottom: usize) -> bool {
+fn row_range_is_reflection(matrix: &Vec<Vec<char>>, mut top: usize, mut bottom: usize, smudges: i32) -> bool {
     if (bottom - top) % 2 == 0 {
         // This means that the "center" of this range is a single row.
         // The center of a reflection must be _between_ two rows though, so
         // this range cannot be valid.
         return false;
     }
+
+    let mut remaining = smudges;
     while top < bottom {
-        if !rows_equal(matrix, top, bottom) {
+        remaining = rows_equal(matrix, top, bottom, remaining);
+        if remaining < 0 {
             return false;
         }
         top += 1;
         bottom -= 1;
     }
-    true
+    remaining == 0
 }
 
-fn rows_equal(matrix: &Vec<Vec<char>>, top: usize, bottom: usize) -> bool {
+fn rows_equal(matrix: &Vec<Vec<char>>, top: usize, bottom: usize, smudges: i32) -> i32 {
+    let mut remaining = smudges;
     for col in 0..matrix[0].len() {
         if matrix[top][col] != matrix[bottom][col] {
-            return false;
+            if remaining == 0 {
+                return -1;
+            }
+            remaining -= 1;
         }
     }
-    true
+    remaining
 }
 
 #[cfg(test)]
@@ -171,6 +201,23 @@ mod day1_tests {
     #[test]
     fn samples_part2() {
         let solution = Solution {};
-        assert_eq!(solution.part_two(""), "");
+        assert_eq!(
+            solution.part_two("#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#"
+            ),
+            "400");
     }
 }
