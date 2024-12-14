df61f9f440eb1f952e67539401d9a3e5a1ea45df
Eric Rinkus <ejrinkus@gmail.com>
2024-12-13 21:54:06 -0700

    day 6 part 2

--- aoc2024/src/days/day6.zig
+++ aoc2024/src/days/day6.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 const ArrayList = std.ArrayList;
+const AutoHashMap = std.AutoHashMap;
 
 const Guard = struct {
     row: usize = 0,
@@ -11,13 +12,16 @@ const Guard = struct {
 pub fn partOne(allocator: Allocator, input: ArrayList([]u8)) ![]const u8 {
     var sum: u32 = 1;
 
+    // Initialize our guard in their starting position.
     var guard: Guard = findGuard(input);
     if (guard.dir == '#') {
         std.debug.print("Couldn't find guard starting position.\n", .{});
         return "";
     }
-    input.items[guard.row][guard.col] = 'X';
 
+    // Repeatedly move the guard, updating the grid every time they visit a new location (and
+    // increment our sum every time they do visit a new location).
+    input.items[guard.row][guard.col] = 'X';
     while (moveGuard(input, &guard)) {
         if (input.items[guard.row][guard.col] == 'X') {
             continue;
@@ -30,10 +34,123 @@ pub fn partOne(allocator: Allocator, input: ArrayList([]u8)) ![]const u8 {
 }
 
 pub fn partTwo(allocator: Allocator, input: ArrayList([]u8)) ![]const u8 {
-    var sum: usize = 0;
-    sum += input.items.len;
+    var sum: u32 = 0;
+
+    // Initialize our guard in their starting position.
+    var guard: Guard = findGuard(input);
+    if (guard.dir == '#') {
+        std.debug.print("Couldn't find guard starting position.\n", .{});
+        return "";
+    }
+
+    // To avoid repeated reallocations of simulated grids, we'll initialize one grid up front and
+    // then just memcpy over it as needed.
+    var sim_grid = ArrayList([]u8).init(allocator);
+    defer {
+        for (sim_grid.items) |slice| {
+            allocator.free(slice);
+        }
+        sim_grid.deinit();
+    }
+    for (input.items) |slice| {
+        const new_slice = try allocator.alloc(u8, slice.len);
+        @memcpy(new_slice, slice);
+        try sim_grid.append(new_slice);
+    }
+
+    // The grid is initially populated with ascii symbols, which all have a decimal value greater
+    // than 32.  Additionally, our traversal bitmask is only 4 bits wide (values 0-15 decimal).
+    // So if we want to see if a location was previously traversed, we can just check that it's
+    // less than 16.
+    input.items[guard.row][guard.col] = dirMask(guard.dir);
+
+    var prev_guard = Guard{
+        .row = guard.row,
+        .col = guard.col,
+        .dir = guard.dir,
+    };
+    var should_test: bool = undefined;
+    while (moveGuard(input, &guard)) {
+        if (input.items[guard.row][guard.col] >= 16) {
+            // If this is a novel location, set it to the guard's current direction.
+            // This also means we need to test placing an obstacle here.
+            input.items[guard.row][guard.col] = dirMask(guard.dir);
+            should_test = true;
+        } else {
+            // Otherwise, OR the guard's current direction into the existing mask.
+            // Don't test an obstacle here, since we would've already done it earlier on the path
+            // (i.e. placing it earlier would've already altered the path).
+            input.items[guard.row][guard.col] |= dirMask(guard.dir);
+            should_test = false;
+        }
+
+        if (guard.dir != prev_guard.dir) {
+            // We just turned, ensure this direction is a part of the previous spot's mask.
+            input.items[prev_guard.row][prev_guard.col] |= dirMask(guard.dir);
+        }
+
+        if (!should_test) {
+            // Reset prev_guard to the current state of the guard for the next iteration.
+            prev_guard.row = guard.row;
+            prev_guard.col = guard.col;
+            prev_guard.dir = guard.dir;
+            continue;
+        }
+
+        // Copy the updated grid and run our simulation.
+        for (input.items, sim_grid.items) |slice, sim_slice| {
+            @memcpy(sim_slice, slice);
+        }
+
+        // First, set the guard's current location to an obstacle.  Then, simulate prev_guard's
+        // new path with this obstacle in place to see if we find a loop.
+        sim_grid.items[guard.row][guard.col] = '#';
+        if (findLoop(sim_grid, &prev_guard)) {
+            sum += 1;
+        }
+
+        // Reset prev_guard to the current state of the guard for the next iteration.
+        prev_guard.row = guard.row;
+        prev_guard.col = guard.col;
+        prev_guard.dir = guard.dir;
+    }
 
     return try std.fmt.allocPrint(allocator, "{d}", .{sum});
+}
+
+pub fn findLoop(input: ArrayList([]u8), guard: *Guard) bool {
+    // If the guard is already surrounded by obstacles, we're in a loop.  Return now since
+    // moveGuard could break in this edge case.
+    if (guard.row > 0 and (input.items[guard.row - 1][guard.col] == '#') and
+        guard.col > 0 and (input.items[guard.row][guard.col - 1] == '#') and
+        guard.row < input.items.len - 1 and (input.items[guard.row + 1][guard.col] == '#') and
+        guard.col < input.items[0].len - 1 and (input.items[guard.row][guard.col + 1] == '#'))
+    {
+        return true;
+    }
+
+    // The current location of the guard should already have the correct direction mask, since it's
+    // a location that the guard had already previously visited.  So just start the simulated path.
+    while (moveGuard(input, guard)) {
+        const space = &input.items[guard.row][guard.col];
+        const space_mask = space.*;
+        const guard_mask = dirMask(guard.dir);
+        if (space_mask >= 16) {
+            // This is a novel location.
+            space.* = guard_mask;
+        } else {
+            // This isn't a novel location.  See if we've already traveled through this location in
+            // the same direction before.
+            if ((space_mask & guard_mask) == guard_mask) {
+                // We found a loop.
+                return true;
+            }
+            // No loop, update the direction mask and move on.
+            space.* |= guard_mask;
+        }
+    }
+
+    return false;
 }
 
 fn findGuard(input: ArrayList([]u8)) Guard {
@@ -130,21 +247,12 @@ fn moveGuard(input: ArrayList([]u8), guard: *Guard) bool {
     return true;
 }
 
-fn printState(input: ArrayList([]u8), guard: Guard) void {
-    const height = input.items.len;
-    const width = input.items[0].len;
-
-    for (0..height) |row| {
-        for (0..width) |col| {
-            if (row == guard.row and col == guard.col) {
-                std.debug.print("{c}", .{guard.dir});
-            } else if (input.items[row][col] == '#') {
-                std.debug.print("#", .{});
-            } else {
-                std.debug.print(".", .{});
-            }
-        }
-        std.debug.print("\n", .{});
-    }
-    std.debug.print("\n", .{});
+fn dirMask(dir: u8) u8 {
+    return switch (dir) {
+        '^' => 0b00001000,
+        '>' => 0b00000100,
+        'v' => 0b00000010,
+        '<' => 0b00000001,
+        else => 0b00000000,
+    };
 }
